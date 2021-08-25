d4533bc236c01c0795af30d3dddd1187a05b6f03
arnaldosantosjr <arnaldoleao6@gmail.com>
2021-08-25 17:52:42 -0300

    seu nascimento e soma
    Adicionei mais dois miniprojetos de py

---
+++ apresente-se.py
@@ -0,0 +1,2 @@
+nome = input ('Qual o seu nome?\n')
+print ('Seja bem vindo, ', nome,'!')
---
+++ soma.py
@@ -0,0 +1,4 @@
+n1 = int(input ('Digite um número.\n'))
+n2 = int(input ('Digite outro número.\n'))
+soma = n1 + n2
+print ('A soma entre eles é: ',soma)
